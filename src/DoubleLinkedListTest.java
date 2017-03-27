849d46a3422d6588a2db29c4f555a34caa742249
marble <markusblechschmidt@gmail.com>
2017-03-27 16:45:15 +0200

    moved and implemented test for double linked list

---
+++ src/DoubleLinkedListTest.java
@@ -0,0 +1,97 @@
+/**
+ * Created by marble on 3/26/17.
+ */
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DoubleLinkedListTest {
+    private final int TEST_SIZE = 10;
+    Counter myCounter;
+    DoubleLinkedList<Integer> doubleLinkedList;
+    Elem<Integer> temp;
+    boolean excpetion_handled = false;
+
+    @Before
+    public void setup(){
+        excpetion_handled = false;
+        doubleLinkedList = new DoubleLinkedList<>();
+        /* fill list with dummy data */
+        for(int i = 0; i < TEST_SIZE; i++) {
+            temp = new Elem<Integer>();
+            temp.setData(i);
+            doubleLinkedList.insert(new Pos(i), temp);
+        }
+    }
+
+    @Test
+    public void testDoubleLinkedListInsert(){
+        /* check first, middle and last element */
+        assertEquals(doubleLinkedList.retrieve(new Pos(0)).getData(), (Integer)(0));
+        assertEquals(doubleLinkedList.retrieve(new Pos(TEST_SIZE/2)).getData(), (Integer)(TEST_SIZE/2));
+        assertEquals(doubleLinkedList.retrieve(new Pos(TEST_SIZE-1)).getData(), (Integer)(TEST_SIZE-1));
+        /* try to insert after end */
+        try {
+            temp = new Elem<Integer>();
+            temp.setData(TEST_SIZE+1);
+            doubleLinkedList.insert(new Pos(TEST_SIZE+1), temp);
+        } catch(IndexOutOfBoundsException e){
+            excpetion_handled = true;
+        }
+        assertTrue(excpetion_handled);
+        /* try to insert before start */
+        try {
+            temp = new Elem<Integer>();
+            temp.setData(-1);
+            doubleLinkedList.insert(new Pos(-1), temp);
+        } catch(IndexOutOfBoundsException e){
+            excpetion_handled = true;
+        }
+        assertTrue(excpetion_handled);
+    }
+
+    @Test
+    public void testDoubleLinkedListDelete(){
+        /* delete first, middle and last element and check size */
+        doubleLinkedList.delete(new Pos(TEST_SIZE-1));
+        doubleLinkedList.delete(new Pos(TEST_SIZE/2));
+        doubleLinkedList.delete(new Pos(0));
+        /* try to delete two after end */
+        try {
+            doubleLinkedList.delete(new Pos(TEST_SIZE));
+        } catch(IndexOutOfBoundsException e){
+            excpetion_handled = true;
+        }
+        assertTrue(excpetion_handled);
+        /* try to delete before start */
+        try {
+            doubleLinkedList.delete(new Pos(-1));
+        } catch(IndexOutOfBoundsException e){
+            excpetion_handled = true;
+        }
+        assertTrue(excpetion_handled);
+    }
+
+    @Test
+    public void testDoubleLinkedListFind(){
+        Elem<Integer> temp= new Elem<Integer>();
+        try {
+            temp.setData(0);
+            assertEquals(doubleLinkedList.find(temp).getPos(), 0);
+            temp.setData(TEST_SIZE/2);
+            assertEquals(doubleLinkedList.find(temp).getPos(), TEST_SIZE/2);
+            temp.setData(TEST_SIZE-1);
+            assertEquals(doubleLinkedList.find(temp).getPos(), TEST_SIZE-1);
+        } catch(ElementNotFoundException e){
+            excpetion_handled = true;
+        }
+        assertFalse(excpetion_handled);
+        /* try to find non existing element */
+        try {
+            temp.setData(Integer.MIN_VALUE);
+            doubleLinkedList.find(temp);
+        } catch(ElementNotFoundException e){
+            excpetion_handled = true;
+        }
+        assertTrue(excpetion_handled);
+    }
+}
