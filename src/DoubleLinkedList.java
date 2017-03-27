30d24f850ceefb3ada380574067d919b7123ad26
marble <markusblechschmidt@gmail.com>
2017-03-27 21:27:16 +0200

    added count(); instructions

--- src/DoubleLinkedList.java
+++ src/DoubleLinkedList.java
@@ -9,12 +9,18 @@ public class DoubleLinkedList<T> implements OwnList{
 
     DoubleLinkedList() {
         headDummy = new Elem<>();
+        count();
         lastDummy = new Elem<>();
+        count();
         headDummy.setNext(lastDummy);
+        count();
         lastDummy.setPrev(headDummy);
+        count();
         /* make loops at the end */
         headDummy.setPrev(headDummy);
+        count();
         lastDummy.setNext(lastDummy);
+        count();
     }
 
     DoubleLinkedList(Counter counter) {
@@ -25,58 +31,89 @@ public class DoubleLinkedList<T> implements OwnList{
     @Override
     public void insert(Pos insertPos, Elem insertElem) throws IndexOutOfBoundsException {
         Elem<T> refElem = insertPos.getRefElem();
+        count();
         if(refElem == null) {
             refElem = retrieve(insertPos);
+            count();
         }
+        count();
         /* attach insert element after predecessor of reference element */
         insertElem.setPrev(refElem.getPrev());
+        count();
         refElem.getPrev().setNext(insertElem);
+        count();
         /* attach reference element after insert element */
         refElem.setPrev(insertElem);
+        count();
         insertElem.setNext(refElem);
+        count();
         size++;
+        count();
     }
 
     @Override
     public void delete(Pos deletePos) throws IndexOutOfBoundsException {
         Elem<T> refElem = deletePos.getRefElem();
+        count();
         if(refElem == null) {
             refElem = retrieve(deletePos);
+            count();
         }
+        count();
         refElem.getPrev().setNext(refElem.getNext());
+        count();
         refElem.getNext().setPrev(refElem.getPrev());
+        count();
         size--;
+        count();
     }
 
     @Override
     public Pos find(Elem findElem) throws ElementNotFoundException {
         Pos resultPos = new Pos(headDummy);
+        count();
         lastDummy.setData((T)findElem.getData());
+        count();
         while(!resultPos.getRefElem().equals(findElem)) {
             resultPos.setRefElem(resultPos.getRefElem().getNext());
+            count();
             resultPos.set(resultPos.getPos()+1);
+            count();
+            count();
         }
+        count();
         if(resultPos.getRefElem() == lastDummy) {
             lastDummy.setData(null);
+            count();
             throw new ElementNotFoundException();
         }
+        count();
         lastDummy.setData(null);
+        count();
         return resultPos;
     }
 
     @Override
     public Elem retrieve(Pos retrievePos) throws IndexOutOfBoundsException {
         Elem<T> result = retrievePos.getRefElem();
+        count();
         final int pos = retrievePos.getPos();
+        count();
         if(result == null) {
             if(pos > size || pos < 0){
                 throw new IndexOutOfBoundsException();
             }
+            count();
             result = headDummy.getNext();
+            count();
             for(int i = 0; i < pos; i++) {
                 result = result.getNext();
+                count();
+                count();
             }
+            count();
         }
+        count();
         return result;
     }
 
@@ -84,13 +121,18 @@ public class DoubleLinkedList<T> implements OwnList{
     public void concat(OwnList otherList) {
         if (otherList.getClass().equals(this.getClass())) {
             lastDummy.getPrev().setNext(((DoubleLinkedList) otherList).headDummy.getNext());
+            count();
             headDummy.getNext().setPrev(((DoubleLinkedList) otherList).lastDummy.getPrev());
+            count();
             lastDummy = ((DoubleLinkedList) otherList).lastDummy;
+            count();
             size += otherList.size();
+            count();
         }
         else {
             /* TODO implement */
         }
+        count();
     }
 
     @Override
